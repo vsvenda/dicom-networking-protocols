eddfe25aab9240a0ccbe7c3e5646650ae3433c7d
vsvenda <125673492+vsvenda@users.noreply.github.com>
2024-02-17 11:31:21 +0100

    Adding scp code

--- README.md
+++ README.md
@@ -3,13 +3,22 @@
 This code goes through the general workflow of dicom network protocols through pynetdicom.
 pydicom: https://pydicom.github.io/pynetdicom/stable/index.html#pynetdicom
 
-You should be able to connect to a server of your liking, but if you do not have one use echoscp which comes with pynetdicom:
+You should be able to connect to a scp of your liking, but if you do not have one use echoscp which comes with pynetdicom:
 $ python -m pynetdicom echoscp 11112 -v
 
 ** my_scu.py **
 Used to create a SCU application entity, associate it to the SCP and request its verification service.
-For more info see: https://pydicom.github.io/pynetdicom/stable/tutorials/create_scu.html
+
+For more info acout SCU see: https://pydicom.github.io/pynetdicom/stable/tutorials/create_scu.html
+------------------------------------------------------------------------------------------------------------------------
 
 ** my_scp.py **
+Used to create a SCP application entity.
 
-For more info see: https://pydicom.github.io/pynetdicom/stable/tutorials/create_scp.html
+To start the SCP:
+$ python my_scp.py
+
+To send dicom file:
+$ python -m pynetdicom storescu 127.0.0.1 11112 CTImageStorage.dcm -v -cx
+
+For more info about SCP see: https://pydicom.github.io/pynetdicom/stable/tutorials/create_scp.html
--- my_scp.py
+++ my_scp.py
@@ -1,10 +1,33 @@
-from pydicom.uid import ExplicitVRLittleEndian, CTImageStorage
+from pydicom.uid import ExplicitVRLittleEndian
 
-from pynetdicom import AE, debug_logger
+from pynetdicom import (
+    AE, debug_logger, evt,
+    AllStoragePresentationContexts,
+    ALL_TRANSFER_SYNTAXES
+)
+
 from pynetdicom.sop_class import CTImageStorage
 
 debug_logger()
 
-ae = AE()
-ae.add_supported_context(CTImageStorage, ExplicitVRLittleEndian)  # add *supported* presentation context
-ae.start_server(("127.0.0.1", 11112), block=True)  # start listening for association requests on port 11112
+def handle_store(event):  # need to define handlers for EVT_C_STORE (what C_STORE should do)
+    """Handle EVT_C_STORE events."""
+    ds = event.dataset  # decoded dataset from the SCU received as a pydicom dataset
+    ds.file_meta = event.file_meta  # meta information
+    ds.save_as(ds.SOPInstanceUID, write_like_original=False)  # save it to a file named after its (0008,0018) SOP Instance UID,
+    return 0x0000
+
+handlers = [(evt.EVT_C_STORE, handle_store)]
+
+ae = AE()  # create AE (application entity) instance
+
+# define handling of all the storage service’s SOP Classes by adding more supported presentation contexts
+storage_sop_classes = [
+    # AllStoragePresentationContexts is a list of pre-built presentation contexts, one for every SOP Class in the storage service
+    cx.abstract_syntax for cx in AllStoragePresentationContexts
+]
+for uid in storage_sop_classes:
+    # ALL_TRANSFER_SYNTAXES supports both compressed and uncompressed transfer syntax
+    ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)  # add *supported* presentation context
+
+ae.start_server(("127.0.0.1", 11112), block=True, evt_handlers=handlers)  # start listening for association requests on port 11112
